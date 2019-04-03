b5432f560364426a7bcd0fae29d75e52a555e2cc
byrontribs <byrontribs@yahoo.com>
2019-04-03 14:32:02 +0200

    functionallity for endless scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,33 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= //= require foundation
-require_tree .
+//= require jquery
+//= require foundation
+//= require_tree
 
 $(function(){ $(document).foundation(); });
+
+$( document ).ready(function() {
+    var PageNumber = 2;
+    var running = false;
+
+    $(window).bind('scroll', function() {
+        if($(window).scrollTop() >= $('.product-container').offset().top + $('.product-container').outerHeight() - window.innerHeight) {
+          if(running == false){
+            $('.loading-gif').fadeIn();
+            running = true;
+            $.ajax({
+              type: "GET",
+              url: "/all_products?page=" + PageNumber,
+              data: $(this).serialize(),
+              success: function(response) {
+                $('.loading-gif').fadeOut();
+                $('.js-all-products').append(response.productsPartial);
+              }
+            });
+            PageNumber = PageNumber + 1
+            setTimeout(function(){running = false}, 300);
+          }
+        }
+    });
+});
