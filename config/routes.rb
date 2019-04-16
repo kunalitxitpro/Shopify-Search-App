498b98aaf745786dedb5f46fac76ce22c493de44
byrontribs <byrontribs@yahoo.com>
2019-04-16 14:14:37 +0200

    changed route name to index

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   namespace :app_proxy do
     root action: 'index'
-    get 'allprods', to: 'app_proxy#index'
+    # get 'allprods', to: 'app_proxy#index'
+    get 'index', to: 'app_proxy#index'
     # simple routes without a specified controller will go to AppProxyController
 
     # more complex routes will go to controllers in the AppProxy namespace
