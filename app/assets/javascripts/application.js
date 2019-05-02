42e9b5fe02126aa3a68108a1cac104a976723757
byrontribs <byrontribs@yahoo.com>
2019-05-02 16:32:37 +0200

    set up for search results index,
    routes
    model for view more in index
    view logic for params

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -245,6 +245,40 @@ $(document).on('turbolinks:load', () => {
       $(this).parent(".main-search").toggleClass("collapsed");
     });
 
+
+    $('.quick-view-button').click(function() {
+      $('.the-modal').show();
+    });
+
+    $('.model-close').click(function() {
+      $('.the-modal').hide();
+    });
+
+    // var modal = document.getElementById('myModal');
+    //
+    // // Get the button that opens the modal
+    // var btn = document.getElementById("myBtn");
+    //
+    // // Get the <span> element that closes the modal
+    // var span = document.getElementsByClassName("close")[0];
+    //
+    // // When the user clicks the button, open the modal
+    // btn.onclick = function() {
+    //   modal.style.display = "block";
+    // }
+    //
+    // // When the user clicks on <span> (x), close the modal
+    // span.onclick = function() {
+    //   modal.style.display = "none";
+    // }
+    //
+    // // When the user clicks anywhere outside of the modal, close it
+    // window.onclick = function(event) {
+    //   if (event.target == modal) {
+    //     modal.style.display = "none";
+    // }
+
+
     window.tabs = new function() {
       this.loadTabs = function() {
 
--- app/controllers/app_proxy_controller.rb
+++ app/controllers/app_proxy_controller.rb
@@ -16,7 +16,7 @@ class AppProxyController < ApplicationController
 
   def render_all_products
     shop = Shop.find_by(shopify_domain: params[:shop])
-    @products = ProductSearch.new(product_params, shop).search
+    @products = Product.all.limit(10)
     @vendor_array = ['Adidas', 'American Sports Teams','Aquascutum','Armani','Asics', 'Avirex', 'Barbour', 'Belstaff', 'Best Company', 'Burberry', 'Nike']
     @size_array = ['L', 'M', 'S', "Women's", 'XL', 'XS', 'XXL', 'XXS']
     @product_type = ['Bags', 'dress', 'Football shirts', 'Hockey Top', 'Jackets & Coats', 'Jeans', 'Party Shirts', 'Polo Shirts', 'Rugby Tops', 'Shirts', 'Shorts', 'Skirts', 'Sweatshirts & Hoods', 'T-Shirts', 'Tracksuit', 'Tracksuit Bottoms', 'Trainers', 'Vests', 'Wholesale']
