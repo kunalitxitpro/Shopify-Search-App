2e238876f1be14887c49b1b7e020665598a31aeb
byrontribs <byrontribs@yahoo.com>
2019-05-02 13:54:15 +0200

    semi-polish style for admin section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -138,19 +138,19 @@ $(document).on('turbolinks:load', () => {
     $('.js-true-scroll-option').click(function(){
       $('.js-admin-section-filter-config-container, .js-admin-section-search-config-container').hide()
       $('.js-admin-section-scroll-config-container').show()
-      $('.js-true-scroll-option').css({'border-left':'10px solid pink', 'background-color': '#a7b8c9', 'color': 'white'});
+      $('.js-true-scroll-option').css({'border-left':'5px solid rgb(92, 106, 196)', 'background-color': 'white', 'color': 'rgb(92, 106, 196)'});
       $('.js-true-filter-option, .js-true-search-option').css({'border-left':'0', 'background-color': 'transparent', 'color': 'black'})
     });
     $('.js-true-filter-option').click(function(){
       $('.js-admin-section-scroll-config-container, .js-admin-section-search-config-container').hide()
       $('.js-admin-section-filter-config-container').show();
-      $('.js-true-filter-option').css({'border-left':'10px solid pink', 'background-color': '#a7b8c9', 'color': 'white'});
+      $('.js-true-filter-option').css({'border-left':'5px solid rgb(92, 106, 196)', 'background-color': 'white', 'color': 'rgb(92, 106, 196);'});
       $('.js-true-scroll-option, .js-true-search-option').css({'border-left':'0', 'background-color': 'transparent', 'color': 'black'})
     });
     $('.js-true-search-option').click(function(){
       $('.js-admin-section-scroll-config-container, .js-admin-section-filter-config-container').hide()
       $('.js-admin-section-search-config-container').show();
-      $('.js-true-search-option').css({'border-left':'10px solid pink', 'background-color': '#a7b8c9', 'color': 'white'});
+      $('.js-true-search-option').css({'border-left':'5px solid rgb(92, 106, 196)', 'background-color': 'white', 'color': 'rgb(92, 106, 196);'});
       $('.js-true-filter-option, .js-true-scroll-option').css({'border-left':'0', 'background-color': 'transparent', 'color': 'black'})
     });
 
