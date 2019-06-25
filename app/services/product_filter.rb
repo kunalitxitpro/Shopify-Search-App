85e34f2d267f5505e569149fb63b0edad1a12f27
Matthew Riemer <mattriemer36@gmail.com>
2019-06-25 12:00:00 +0200

    fix: did final fixes for launch

--- app/services/product_filter.rb
+++ app/services/product_filter.rb
@@ -7,7 +7,7 @@ class ProductFilter
   def search
     products = Product
     @params[:title].split(' ').each do |query|
-      products = products.where('lower(title) ~* ? OR lower(vendor) ~* ?', query.downcase, query.downcase).where('quantity > 0')
+      products = products.where('lower(title) ~* ? OR lower(vendor) ~* ?', query.downcase, query.downcase).where('quantity > 0').order(price: :desc)
     end
     if ProductSetting.last.overflow_scroll_on
       products.limit(50)
@@ -19,7 +19,7 @@ class ProductFilter
   def popular
     products = Product
     @params[:title].split(' ').each do |query|
-      products = products.where('lower(title) ~* ? OR lower(vendor) ~* ?', query.downcase, query.downcase).where('quantity > 0')
+      products = products.where('lower(title) ~* ? OR lower(vendor) ~* ?', query.downcase, query.downcase).where('quantity > 0').order(price: :desc)
     end
     products.order(quantity: :asc).limit(8)
   end
