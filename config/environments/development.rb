e2b95cab3dfbb6a324169064b242181a5bfdc620
kunalitxitpro <kunal@itxitpro.com>
2019-11-29 16:53:29 +0530

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   config.active_job.queue_adapter = :inline
 
-  config.action_controller.asset_host = "NGROK URL HERE"
+  config.action_controller.asset_host = "http://localhost:3000/"
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
