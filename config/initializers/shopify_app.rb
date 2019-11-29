d68c318a81c26f821e42633de718e65250fbeaf9
kunalitxitpro <55884684+kunalitxitpro@users.noreply.github.com>
2019-11-29 19:03:50 +0530

    Update shopify_app.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,12 +1,12 @@
 ShopifyApp.configure do |config|
-  config.application_name = "My Shopify App"
-  config.api_key = ENV['SHOPIFY_API_KEY']
-  config.secret = ENV['SHOPIFY_API_SECRET']
+  config.application_name = "Bensearch"
+  config.api_key = "f3fc481017cfb0ac799b1b95868efda8"
+  config.secret = "ea565f8ab58f7b2925b5d354d33a826a"
   config.old_secret = "<old_secret>"
   config.scope = "read_products, write_script_tags" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.scripttags = [
-    {event:'onload', src: '#{APP_URL_HERE}/searching.js'}
+    {event:'onload', src: 'https://s3.amazonaws.com/shopify-app-prod/searching.js'}
   ]
   config.embedded_app = true
   config.after_authenticate_job = false
